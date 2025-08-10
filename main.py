b92af2bc16bd1bda14902836bc84dacfdc1dc8a3
Yashesha Kothari <yasheshak@yasheshas-mbp.mynetworksettings.com>
2025-08-10 15:53:56 -0700

    added conc profs and cycle comments

--- main.py
+++ main.py
@@ -6,14 +6,28 @@ import matplotlib as plt
 
 #use gaussian/normal distribution to define continuous concentration field representing food source
 #gradient maximum = (0,0) = highest food concentration
-def food_source(x0 = 0, y0 = 0, x, y, sigma = 3):
+def food_source(x, y, x0 = 0, y0 = 0, sigma = 3):
   return np.exp(-((x-x0)**2 + (y-y0)**2) / (2 * sigma**2))
 
-#create concentration field gradient
+#create concentration field gradient, 3 profiles total
+#picked numbers
+def conc_exponential(x, y, alpha=0.5):
+  r = np.sqrt(x*x + y*y)
+  return np.exp(-alpha * r)
+
+def conc_linear(x, y, k = 0.2):
+  return k * x
 
 
 #define E.coli class
 class Ecoli:
+  
+    # Each cycle:
+    #1) Tumble: 4 small unbiased steps --> monte carlo
+    #2) Sense: compare C(t) vs C(t-4Δt)
+    #3) Run: move along recent heading if improved, else opposite --> gradient step
+    
+
   def __init__(self):
     self.x = np.random.uniform()  #randomize initial position
     self.Ecolipath = [self.x]     #store positions in list, starting w/ initial position
